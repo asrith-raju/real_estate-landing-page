0a49a160ae3b0372becb2e127502268896d3c266
asrith-raju <asrithraju143@gmail.com>
2025-11-08 20:34:03 +0530

    feat: add formspree email integration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+
 
 const Contact = () => {
+    
+
+
+  const [state, handleSubmit] = useForm("xanawoew");
+  useEffect(() => {
+   if (state.succeeded) {
+      alert("Form Submitted Successfully")
+    //   return <p className='text-center'>Your Message has been sent Successfully</p>; 
+  }
+
+  }, [state.succeeded])
+  
+  
   return (
     <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
       <h1 className='text-2xl sm:text-4xl font-bond mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With us</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Real Stories from Those Who Found Home with us</p>
 
-       <form action="" className='max-w-2xl mx-auto text-gray-600 pt-8'>
+       <form onSubmit={handleSubmit} action="" className='max-w-2xl mx-auto text-gray-600 pt-8'>
         <div className='flex flex-wrap'>
             <div className='w-full md:w-1/2 text-left'>
             Your Name
@@ -21,7 +37,13 @@ const Contact = () => {
             Message 
             <textarea className='w-full border border-gray-300 rounded py-3 px-4 mt-2 h-48 resize-none' name="Message" placeholder='Message'required></textarea>
         </div>
-        <button className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'>Send Message</button>
+        <button 
+          type="submit"
+          disabled={state.submitting}
+          className='bg-blue-600 text-white py-2 px-12 mb-10 rounded'
+        >
+          {state.submitting ? 'Sending...' : 'Send Message'}
+        </button>
        </form>
     </div>
   )
