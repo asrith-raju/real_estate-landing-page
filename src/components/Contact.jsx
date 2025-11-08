347b6cda5780f760adc17efa3507e1acd0a59705
asrith-raju <asrithraju143@gmail.com>
2025-11-08 19:10:36 +0530

    feat: create contact section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='text-center p-6 py-20 lg:px-32 w-full overflow-hidden' id='Contact'>
+      <h1 className='text-2xl sm:text-4xl font-bond mb-2 text-center'>Contact <span className='underline underline-offset-4 decoration-1 under font-light'>With us</span></h1>
+      <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Real Stories from Those Who Found Home with us</p>
+
+       
+    </div>
+  )
+}
+
+export default Contact
